308b78558eebec2173466b36ebc5c68ba3cb5c73
kevinfan <kevin.fan@imediplus.com>
2026-01-22 10:18:27 +0800

    chore: Add debug logging for Trello initialization

--- src/services/trello.ts
+++ src/services/trello.ts
@@ -1,24 +1,32 @@
 export const initializePowerUp = () => {
-  if (window.TrelloPowerUp) {
-    window.TrelloPowerUp.initialize({
-      'board-buttons': function (_t: any) {
-        return [{
-          icon: {
-            dark: './vite.svg',
-            light: './vite.svg',
-          },
-          text: 'Checklist Viewer',
-          callback: function (t: any) {
-            return t.modal({
-              title: 'Checklist Viewer',
-              url: window.location.href,
-              fullscreen: true,
-            });
-          },
-        }];
-      },
-    });
+  console.log('Trello Power-Up: Attempting initialization...');
+  if (!window.TrelloPowerUp) {
+    console.error('Trello Power-Up: window.TrelloPowerUp is undefined!');
+    return;
   }
+
+  console.log('Trello Power-Up: window.TrelloPowerUp found. Initializing...');
+  window.TrelloPowerUp.initialize({
+    'board-buttons': function (_t: any) {
+      console.log('Trello Power-Up: board-buttons requested');
+      return [{
+        icon: {
+          dark: './vite.svg',
+          light: './vite.svg',
+        },
+        text: 'Checklist Viewer',
+        callback: function (t: any) {
+          console.log('Trello Power-Up: Button clicked');
+          return t.modal({
+            title: 'Checklist Viewer',
+            url: window.location.href,
+            fullscreen: true,
+          });
+        },
+      }];
+    },
+  });
+  console.log('Trello Power-Up: Initialization called.');
 };
 
 export const t = window.TrelloPowerUp ? window.TrelloPowerUp.iframe() : null;
